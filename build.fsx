0b6a17a3f2d25898d6cf02353583c0d3f503ae0d
Alex Chilcott <alexchilcott@gmail.com>
2015-11-07 12:21:24 +0000

    Get rid of ilmerge for now.
    ILMerge doesn’t work with Mono. Investigate using IL-Repack as an
    alternative

--- build.fsx
+++ build.fsx
@@ -3,41 +3,24 @@
 #r "FakeLib.dll"
 open Fake
 
-//System.Environment.CurrentDirectory <- """C:\Users\alexchilcott\Desktop\automator"""
-
 // Properties
-let buildDir = "./build/"
-let tempDir = combinePaths buildDir "temp"
+let buildDir = "." </> "build" </> ""
 
 // Targets
 Target "Clean" (fun _ ->
     CleanDir buildDir
 )
 
-Target "BuildApp" (fun _ ->
+Target "Build" (fun _ ->
     !! "**/*.fsproj"
-      |> MSBuildRelease tempDir "Build"
+      |> MSBuildRelease buildDir "Build"
       |> Log "AppBuild-Output: "
 )
 
-Target "IlMerge" (fun _ -> 
-    ILMerge
-        (fun x -> {x with TargetKind = TargetKind.Exe; Libraries = System.IO.Directory.EnumerateFiles(tempDir, "*.dll")})
-        (combinePaths buildDir "Autonote-merged.exe")
-        (combinePaths tempDir "Autonote.exe")
-)
-
-Target "Tidy" (fun _ -> 
-    DeleteDir tempDir
-)
-
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
-)
+Target "Default" (fun _ -> ())
 
 // Dependencies
-"Clean" ==> "BuildApp" ==> "IlMerge" ==> "Tidy" ==> "Default"
-
+"Clean" ==> "Build" ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
